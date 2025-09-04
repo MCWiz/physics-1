2eb95377185f27313971c8218f6bf3552b12c792
MCWiz <logues2006@gmail.com>
2025-09-04 14:09:25 -0400

    Changed some text and added 2 moving circles and reformatted a little

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -7,26 +7,49 @@ See documentation here: https://www.raylib.com/, and examples here: https://www.
 #include "raymath.h"
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
+#include "game.h"
 
 const unsigned int TARGET_FPS = 50;
+float dt = 1.0f / TARGET_FPS;
 float time = 0;
+float x = 500;
+float y = 500;
+float frequency = 1;
+float amplitude = 100;
+
+void update()
+{
+    dt = 1.0f / TARGET_FPS;
+    time += dt;
+
+    x = x + (-sin(time * frequency)) * frequency * amplitude * dt;
+    y = y + (cos(time * frequency)) * frequency * amplitude * dt;
+}
+
+void draw()
+{
+    BeginDrawing();
+    ClearBackground(BLACK);
+    DrawText("Logan Christopher Medina 101538952", 10, float(GetScreenHeight() - 30), 20, LIGHTGRAY);
+
+    GuiSliderBar(Rectangle{ 35, 15, 1000, 10 }, "Time", TextFormat("%.2f", time), &time, 0, 240);
+    DrawText(TextFormat("T: %.2f", time), GetScreenWidth() - 130, 10, 30, LIGHTGRAY);
+
+    DrawCircle(x, y, 70, RED);
+    DrawCircle(500 + cos(time * frequency) * amplitude, 500 + sin(time * frequency) * amplitude, 70, GREEN);
+
+    EndDrawing();
+}
+
 int main()
 {
-    InitWindow(1200, 800, "Physics-1");
+    InitWindow(InitialWidth, InitialHeight, "GAME2005 Logan Medina 101538952");
     SetTargetFPS(TARGET_FPS);
 
     while (!WindowShouldClose())
     {
-        BeginDrawing();
-            ClearBackground(WHITE);
-            DrawText("Hello world!", 10, 10, 20, LIGHTGRAY);
-
-
-            time += 1;
-            GuiSliderBar(Rectangle{ 60, 5, 1000, 10 }, "Time", TextFormat("%.2f", time), &time, 0, 240);
-
-
-        EndDrawing();
+        update();
+        draw();
     }
 
     CloseWindow();
