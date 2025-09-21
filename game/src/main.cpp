202202425f6828c5983f7da3169ffdb5f4e13f71
MCWiz <logues2006@gmail.com>
2025-09-21 14:26:42 -0400

    Added Launch Info Depiction and Sliders
    - Added variables for launch start position, velocity, speed, and angle.
    - Added sliders to adjust the launch variables.
    - Calculated velocity using speed and angle.
    - Drew a line to show the start position and velocity.

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -17,6 +17,11 @@ float y = 500;
 float frequency = 1;
 float amplitude = 100;
 
+float speed = 100;
+float angle = 0;
+float startPosX = 100;
+float startPosY = 100;
+
 void update()
 {
     dt = 1.0f / TARGET_FPS;
@@ -32,11 +37,23 @@ void draw()
     ClearBackground(BLACK);
     DrawText("Logan Christopher Medina 101538952", 10, float(GetScreenHeight() - 30), 20, LIGHTGRAY);
 
-    GuiSliderBar(Rectangle{ 35, 15, 1000, 10 }, "Time", TextFormat("%.2f", time), &time, 0, 240);
+    GuiSliderBar(Rectangle{ 35, 15, 1000, 20 }, "Time", TextFormat("%.2f", time), &time, 0, 240);
+
+    GuiSliderBar(Rectangle{ 35, 45, 500, 20 }, "Speed", TextFormat("Speed: %.0f", speed), &speed, -300, 300);
+    GuiSliderBar(Rectangle{ 35, 75, 500, 20 }, "Angle", TextFormat("Angle: %.0f Degrees", angle), &angle, -180, 180);
+
+    GuiSliderBar(Rectangle{ 650, 45, 500, 20 }, "Start", TextFormat("X: %.0f", startPosX), &startPosX, 200, 1000);
+    GuiSliderBar(Rectangle{ 650, 75, 500, 20 }, "Start", TextFormat("Y: %.0f", startPosY), &startPosY, 200, 600);
+
     DrawText(TextFormat("T: %.2f", time), GetScreenWidth() - 130, 10, 30, LIGHTGRAY);
 
-    DrawCircle(x, y, 70, RED);
-    DrawCircle(500 + cos(time * frequency) * amplitude, 500 + sin(time * frequency) * amplitude, 70, GREEN);
+    Vector2 startPos = { startPosX, startPosY };
+    Vector2 velocity = { speed * cos(angle * DEG2RAD), speed * sin(angle * DEG2RAD) };
+
+    DrawLineEx(startPos, startPos + velocity, 3, RED);
+
+    /*DrawCircle(x, y, 70, RED);
+    DrawCircle(500 + cos(time * frequency) * amplitude, 500 + sin(time * frequency) * amplitude, 70, GREEN);*/
 
     EndDrawing();
 }
