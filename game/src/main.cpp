04442b9e3949983696fa3bc865e139645cc94845
MCWiz <logues2006@gmail.com>
2025-10-16 16:54:03 -0400

    Lab Exercise 4

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -31,7 +31,8 @@ float angle = 0;
 class PhysicsObj
 {
 public:
-    Vector2 position = { 500, 500 };
+    bool isStatic = false;
+    Vector2 position = { 0, 0 };
     Vector2 velocity = { 0, 0 };
     float mass = 1;
     Color color = GREEN;
@@ -41,6 +42,8 @@ public:
     {
         DrawCircle(position.x, position.y, 2, color);
     }
+
+    virtual PhysicsShape shape() = 0;
 };
 
 class PhysicsCircle : public PhysicsObj
@@ -53,6 +56,51 @@ public:
         DrawLineEx(position, position + velocity, 3, RED);
 
         DrawCircle(position.x, position.y, radius, color);
+    }
+
+    PhysicsShape shape() override
+    {
+        return CIRCLE;
+    }
+};
+
+class PhysicsHalfspace : public PhysicsObj
+{
+private:
+    float rotation = 0;
+    Vector2 normal = { 0, -1 };
+
+public:
+    void SetRotationInDeg(float rotationInDeg)
+    {
+        rotation = rotationInDeg;
+        normal = Vector2Rotate({ 0, -1 }, rotation * DEG2RAD);
+    }
+
+    float GetRotationInDeg()
+    {
+        return rotation;
+    }
+
+    Vector2 GetNormal()
+    {
+        return normal;
+    }
+
+
+    void draw() override
+    {
+        DrawCircle(position.x, position.y, 8, color);
+
+        DrawLineEx(position, position + normal * 30, 1, color);
+
+        Vector2 parallelToSurface = Vector2Rotate(normal, PI * 0.5f);
+        DrawLineEx(position - parallelToSurface * 4000, position + parallelToSurface * 4000, 1, color);
+    }
+
+    PhysicsShape shape() override
+    {
+        return HALF_SPACE;
     }
 };
 
@@ -77,12 +125,28 @@ bool CircleCircleOverlap(PhysicsCircle* circleA, PhysicsCircle* circleB)
     }
 }
 
+bool CircleHalfspaceOverlap(PhysicsCircle* circle, PhysicsHalfspace* halfspace)
+{
+    Vector2 displacementToCircle = circle->position - halfspace->position;
+
+    //return ((Dot product(displacement, normal) < radius)
+
+    float dot = Vector2DotProduct(displacementToCircle, halfspace->GetNormal());
+    Vector2 projectDisplacementOntoNorm = halfspace->GetNormal() * dot;
+
+    DrawLineEx(circle->position, circle->position - projectDisplacementOntoNorm, 1, GRAY);
+    Vector2 midpoint = circle->position - projectDisplacementOntoNorm * 0.5f;
+    DrawText(TextFormat("D: %6.0f", dot), midpoint.x, midpoint.y, 30, GRAY);
+
+    return dot < circle->radius ? true : false;
+}
+
 class PhysicsWorld
 {
 public:
     std::vector<PhysicsObj*> objects;
     Vector2 accelGravity = { 0, 9 };
-    Vector2 startPos = { 500, 500 };
+    Vector2 startPos = { 500, 700 };
 
     void add(PhysicsObj* newObj)
     {
@@ -93,7 +157,14 @@ public:
     {
         for (int i = 0; i < objects.size(); i++)
         {
+            objects[i]->color = GREEN;
+        }
+
+        for (int i = 0; i < objects.size(); i++)
+        {
             PhysicsObj* object = objects[i];
+
+            if (object->isStatic) continue;
 
             object->position = object->position + object->velocity * dt;
             object->velocity = object->velocity + accelGravity * dt;
@@ -109,51 +180,44 @@ public:
             for (int j = i + 1; j < objects.size(); j++)
             {
                 PhysicsObj* objectPointerA = objects[i];
-                PhysicsCircle* circlePointerA = (PhysicsCircle*)objectPointerA;
-
                 PhysicsObj* objectPointerB = objects[j];
-                PhysicsCircle* circlePointerB = (PhysicsCircle*)objectPointerB;
 
-                if (CircleCircleOverlap(circlePointerA, circlePointerB))
+                PhysicsShape shapeOfA = objectPointerA->shape();
+                PhysicsShape shapeOfB = objectPointerB->shape();
+
+                if (shapeOfA == CIRCLE && shapeOfB == CIRCLE)
                 {
-                    objectPointerA->isColliding = true;
-                    objectPointerB->isColliding = true;
-                }
-                else
-                {
-                    if (objectPointerA->isColliding == true && objectPointerB->isColliding == false)
+                    if (CircleCircleOverlap((PhysicsCircle*)objectPointerA, (PhysicsCircle*)objectPointerB))
                     {
-                        break;
-                    }
-                    else if (objectPointerA->isColliding == true && objectPointerB->isColliding == true)
-                    {
-                        objectPointerA->isColliding = false;
-                        objectPointerB->isColliding = false;
-                    }
-                    else
-                    {
-                        objectPointerA->isColliding = false;
-                        objectPointerB->isColliding = false;
+                        objectPointerA->color = RED;
+                        objectPointerB->color = RED;
                     }
                 }
-            }
-        }
-
-        for (int i = 0; i < objects.size(); i++)
-        {
-            if (objects[i]->isColliding == true)
-            {
-                objects[i]->color = RED;
-            }
-            else
-            {
-                objects[i]->color = GREEN;
+                else if (shapeOfA == CIRCLE && shapeOfB == HALF_SPACE)
+                {
+                    
+                    if (CircleHalfspaceOverlap((PhysicsCircle*)objectPointerA, (PhysicsHalfspace*)objectPointerB))
+                    {
+                        objectPointerA->color = RED;
+                        objectPointerB->color = RED;
+                    }
+                }
+                else if (shapeOfA == HALF_SPACE && shapeOfB == CIRCLE)
+                {
+                    if (CircleHalfspaceOverlap((PhysicsCircle*)objectPointerB, (PhysicsHalfspace*)objectPointerA))
+                    {
+                        objectPointerA->color = RED;
+                        objectPointerB->color = RED;
+                    }
+                }
             }
         }
     }
 };
 
 PhysicsWorld world;
+
+PhysicsHalfspace halfspace;
 
 void cleanup()
 {
@@ -203,15 +267,22 @@ void draw()
     ClearBackground(BLACK);
     DrawText("Logan Christopher Medina 101538952", 10, float(GetScreenHeight() - 30), 20, LIGHTGRAY);
 
-    GuiSliderBar(Rectangle{ 35, 15, 1000, 20 }, "Time", TextFormat("%.2f", time), &time, 0, 240);
+    GuiSliderBar(Rectangle{ 75, 15, 1000, 20 }, "Time", TextFormat("%.2f", time), &time, 0, 240);
 
-    GuiSliderBar(Rectangle{ 35, 45, 450, 20 }, "Speed", TextFormat("Speed: %.0f", speed), &speed, -300, 300);
-    GuiSliderBar(Rectangle{ 35, 75, 450, 20 }, "Angle", TextFormat("Angle: %.0f Degrees", angle * -1), &angle, -180, 180);
+    GuiSliderBar(Rectangle{ 75, 45, 450, 20 }, "Speed", TextFormat("Speed: %.0f", speed), &speed, -300, 300);
+    GuiSliderBar(Rectangle{ 75, 75, 450, 20 }, "Angle", TextFormat("Angle: %.0f Degrees", angle), &angle, -180, 180);
 
     GuiSliderBar(Rectangle{ 75, 105, 1000, 20 }, "Acceleration", TextFormat("Gravity: %.0f", world.accelGravity.y), &world.accelGravity.y, -600, 600);
 
-    GuiSliderBar(Rectangle{ 650, 45, 450, 20 }, "Start", TextFormat("X: %.0f", world.startPos.x), &world.startPos.x, 200, 1000);
-    GuiSliderBar(Rectangle{ 650, 75, 450, 20 }, "Start", TextFormat("Y: %.0f", world.startPos.y), &world.startPos.y, 200, 600);
+    GuiSliderBar(Rectangle{ 660, 45, 450, 20 }, "Start", TextFormat("X: %.0f", world.startPos.x), &world.startPos.x, 0, 1200);
+    GuiSliderBar(Rectangle{ 660, 75, 450, 20 }, "Start", TextFormat("Y: %.0f", world.startPos.y), &world.startPos.y, 0, 800);
+
+    GuiSliderBar(Rectangle{ 75, 135, 450, 20 }, "Halfspace X", TextFormat("X: %.0f", halfspace.position.x), &halfspace.position.x, 0, 1200);
+    GuiSliderBar(Rectangle{ 660, 135, 450, 20 }, "Halfspace Y", TextFormat("Y: %.0f", halfspace.position.y), &halfspace.position.y, 0, 800);
+
+    float halfspaceRotation = halfspace.GetRotationInDeg();
+    GuiSliderBar(Rectangle{ 75, 165, 1000, 20 }, "Rotation", TextFormat(": %.0f", halfspace.GetRotationInDeg()), &halfspaceRotation, -360, 360);
+    halfspace.SetRotationInDeg(halfspaceRotation);
 
     DrawText(TextFormat("T: %.2f", time), GetScreenWidth() - 130, 10, 30, LIGHTGRAY);
 
@@ -225,6 +296,8 @@ void draw()
         world.objects[i]->draw();
     }
 
+    /*halfspace.draw();*/
+
     /*DrawCircle(x, y, 70, RED);
     DrawCircle(500 + cos(time * frequency) * amplitude, 500 + sin(time * frequency) * amplitude, 70, GREEN);*/
 
@@ -235,6 +308,10 @@ int main()
 {
     InitWindow(InitialWidth, InitialHeight, "GAME2005 Logan Medina 101538952");
     SetTargetFPS(TARGET_FPS);
+
+    halfspace.isStatic = true;
+    halfspace.position = { 600, 700 };
+    world.add(&halfspace);
 
     while (!WindowShouldClose())
     {
